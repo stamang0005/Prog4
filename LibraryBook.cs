17ea134b3a5835e4a8e354550d1961b1f59e12b1
stamang0005 <63462456+stamang0005@users.noreply.github.com>
2020-04-15 20:37:24 -0400

    Add files via upload

---
+++ LibraryBook.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog4
+{
+    public class LibraryBook
+    {
+        private int _copyright;//validation
+        private bool _checkedout;//bool type
+        public const int Year = 2018;//backing field 
+
+        //initialized with book, author, publisher, copyrightyear, number)
+        public LibraryBook(string book, string author, string publisher, int year, string number)
+        {
+            BookName = book;
+            AuthorName = author;
+            Publisher = publisher;
+            CopyrightYear = year;
+            Number = number;
+
+        }
+
+        public string BookName
+        {
+            get;
+            //precondition not applicable.
+            //postcondtion: return title
+            set;
+        }
+
+        public string AuthorName
+        {
+            //postcondition: author returned.
+            get;
+            //postcondition specified for author.
+            set;
+        }
+
+        public string Publisher
+        {
+            get;
+            //postcondtion returned publisher
+            //postcondtion specified for publisher.
+            set;
+        }
+
+        public int CopyrightYear
+        {
+            //precondtion not applicable
+            //postcondition return copyright
+            get { return _copyright; }
+
+            //precondtion value >0
+            //set to copyright year
+            set
+            {
+                if (value > 0)
+                    _copyright = value;
+                else
+                    _copyright = Year;
+            }
+        }
+        public string Number
+        {
+            //precondition not applicable
+            //return call number
+            get;
+            //vakue set to call  number
+            set;
+        }
+
+
+        //returning properties as string
+        public override string ToString()
+        {
+            return $"Book Name: {BookName} {Environment.NewLine}" +
+                $"Author Name: {AuthorName} {Environment.NewLine}" +
+                $"Publisher: {Publisher} {Environment.NewLine}" +
+                $"Copyright Year: {CopyrightYear}{Environment.NewLine}" +
+                $"Call Number: {Number}{Environment.NewLine}";
+        }
+    }
+  
+
+    //}
+}
---
+++ MainMethod.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog4
+{
+    class MainMethod
+    {
+        static void Main(string[] args)
+        {
+            LibraryBook BookContent1 =
+                new LibraryBook("The City of God", "Augustine", "Cambridge University Press", 1998, "123-456-7890");
+            LibraryBook BookContent2 =
+                new LibraryBook("the masks of God", "Joseph Campbell", "Penguin Books", 1976, "111-111-1111");
+            LibraryBook BookContent3 =
+                new LibraryBook("Arrow of God", "Chinua Achebe", "Anchor books", 1974, "222-222-2222");
+            LibraryBook BookContent4 =
+                new LibraryBook("God made them all", "James Herriot", "St. Martin's Press", 1981, "333-333-3333");
+            LibraryBook BookContent5 =
+                new LibraryBook("Fist of God", "Frederick Forsyth", "Bantam Books", 1994, "444-444-4444");
+
+            LibraryBook[] libraryBooks = new LibraryBook[5];
+
+            //book array
+            libraryBooks[0] = BookContent1;
+            libraryBooks[1] = BookContent2;
+            libraryBooks[2] = BookContent3;
+            libraryBooks[3] = BookContent4;
+            libraryBooks[4] = BookContent5;
+
+            DisplayBook(libraryBooks);
+
+            //pre and post condition use to output books properties to display.
+            void DisplayBook(LibraryBook[] arr)
+            {
+
+                foreach (LibraryBook books in arr)
+                {
+                    Console.WriteLine(books);//displays book
+                   
+                }
+               
+
+            }
+      
+        }
+    }
+}
